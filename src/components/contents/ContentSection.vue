2b11c5450e7530c94eccc1325f9f0ed7c4c48180
PNNutkung <pipatpol.t@ku.th>
2016-11-25 15:05:07 +0700

    make an error 2

---
+++ src/components/contents/ContentSection.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="page-content">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/header/MenuHeader.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <header id="menu-header" class="mdl-layout__header mdl-layout__header--scroll">
+    <div class="mdl-layout__header-row">
+      <span class="mdl-layout-title">{{ title }}</span>
+      <div class="mdl-layout-spacer"></div>
+      <nav class="mdl-navigation" v-for="page in pages">
+        <router-link class="mdl-navigation__link" :to="page.link" >
+          {{ page.name }}
+        </router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import VueRouter from 'vue-router'
+import ContentSection from '../contents/ContentSection'
+export default {
+  name: 'menuHeader',
+  data: function () {
+    return {
+      router,
+      title: 'Data Visualization',
+      pages: [
+        {
+          name: 'Home',
+          link: '/'
+        },
+        {
+          name: 'Data',
+          link: '/data'
+        },
+        {
+          name: 'About',
+          link: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
