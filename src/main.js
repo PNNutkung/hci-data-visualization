5e6919c049755e067b9700555a874eb996a8f401
PNNutkung <pipatpol.t@ku.th>
2016-11-25 18:34:25 +0700

    :arrow_up: Change component name

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-import {router} from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { router } from './router'
 
+Vue.use(VueAxios, axios)
 /* eslint-disable no-new */
 new Vue({
   router,
