179373a0bf8173cdca876bd61b16a3f314f91d27
PNNutkung <pipatpol.t@ku.th>
2016-11-28 16:16:02 +0700

    :arrow_up: Add firebase dependency

--- src/components/contents/Data.vue
+++ src/components/contents/Data.vue
@@ -5,10 +5,20 @@
 </template>
 
 <script>
+import { Converter } from 'csvtojson'
+import fs from 'fs'
+
 export default {
   data () {
     return {
       message: 'Hello data page'
+    }
+  },
+  computed: {
+    parseCSVfile: function () {
+      Converter.fromFile(fs.readFileSync('./../../rawData/average_income_per_household.csv', 'utf-8'), (err, result) => {
+        return result
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App'
-import {router} from './router'
+import { router } from './router'
+import * as firebase from 'firebase'
+
+const config = JSON.parse('./config.json')
 /* eslint-disable no-new */
+firebase.initializeApp(config)
 new Vue({
   router,
   el: '#app',
