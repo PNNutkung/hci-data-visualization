b444696feef085eeab0c558952062bcaaa520717
PNNutkung <pipatpol.t@ku.th>
2016-12-01 21:22:06 +0700

    :ambulance: fix firebase catch-all fallback route

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,19 @@
   <div id="app" v-md-theme="'default'">
     <menuHeader></menuHeader>
     <main>
-      <ContentSection></ContentSection>
+      <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
 import MenuHeader from './components/header/MenuHeader'
-import ContentSection from './components/contents/ContentSection'
 import 'vue-material/dist/vue-material.css'
 
 export default {
   name: 'app',
   components: {
-    MenuHeader,
-    ContentSection
+    MenuHeader
   }
 }
 </script>
